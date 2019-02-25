5f4c7fd7bb51870fa80364e10d2c8ce98a92e1ba
Stuart John Gordon Dow <stuart.dow@blackrock.com>
2019-02-25 20:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  if (animal==="dog") {
+    return animal ("cat")
+  }else {
   return animal
+}
 }
 
 function add2(n) {
